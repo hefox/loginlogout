746bd20643ec29efec1b4837714ee1e1d38d3f7d
Fox <fox@Alexander-Grape-labtop.local>
2010-01-18 22:55:02 -0500

    making it maybe clean urls compatable

--- loginlogout.js
+++ loginlogout.js
@@ -1,10 +1,13 @@
 (function ($) {
 
 function loginlogoutAttach(context) {
-  var destination = Drupal.settings.loginlogout.destination;
-  if (destination) {
-    $("a[href='/logout']").attr('href', '/logout?'+destination);
-    $("a[href='/user/login']").attr('href', '/user/login?'+destination)
+  var login_destination = Drupal.settings.loginlogout.login_destination;
+  var logout_destination = Drupal.settings.loginlogout.logout_destination;
+  if (login_destination && logout_destination) {
+    var login = Drupal.settings.loginlogout.login;
+    var logout = Drupal.settings.loginlogout.logout;
+    $("a[href='"+ login + "']").attr('href', login_destination);
+    $("a[href='"+ logout + "']").attr('href', logout_destination);
   }
 }
 Drupal.behaviors.loginlogout = function(context) {
